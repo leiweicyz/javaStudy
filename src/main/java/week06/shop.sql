aac7c7e9ab4b7e728148472e2d1daab9c30dddd6
leiweicyz <leiweicyz@qq.com>
2021-06-14 23:43:40 +0800

    week06

---
+++ src/main/java/week06/shop.sql
@@ -0,0 +1,37 @@
+
+create table study_member(
+    id int auto_increment not null  primary key,
+    username varchar(20) not null,
+    password varchar(100) not null,
+    read_name varchar(20)
+);
+
+create table study_product(
+    id int auto_increment not null primary key,
+    product_name nvarchar(100),
+    price decimal(18,2),
+    product_img nvarchar(100),
+    intro nvarchar(2000),
+    stock int,
+    product_status int
+);
+
+create table study_order(
+    id int auto_increment not null primary key,
+    order_number varchar(32),
+    member_id int,
+    product_count int,
+    total_price decimal(18,2),
+    create_time datetime,
+    pay_time datetime
+);
+
+create table study_order_item(
+     id int auto_increment not null primary key,
+     product_id int,
+     order_id int,
+     product_count int,
+     unit_price decimal(18,2),
+     total_price decimal(18,2),
+     member_id int
+)
